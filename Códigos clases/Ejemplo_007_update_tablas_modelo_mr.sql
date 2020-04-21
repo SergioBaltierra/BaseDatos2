520a0790e8ca0ef7ac367eaa2a07c73a4a88ec30
SergioBaltierra <sergio.baltierra@gmail.com>
2020-04-21 09:00:25 -0400

    Update y Delete

---
+++ Códigos clases/Ejemplo_007_update_tablas_modelo_mr.sql
@@ -0,0 +1,4 @@
+﻿-- En la tabla estudiante modificamos el nombre del estudiante, cuya PK es 1
+UPDATE ESTUDIANTE 
+SET NOMBRES = 'SACA'
+WHERE ID_ESTUDIANTE = 1
---
+++ Códigos clases/Ejemplo_008_delete_tablas_modelo_mr.sql
@@ -0,0 +1,14 @@
+﻿-- Borrado de una tupla de una tabla, primero hay que borrar las tuplas que hacen referencias
+-- Borramos la tupla cuyo ID_MATRICULADO = 1
+BEGIN TRANSACTION
+DELETE FROM MATRICULADO
+WHERE ID_MATRICULADO = 1;
+
+-- Y borramos la tupla cuyo ID_CURSO = 1
+DELETE FROM CURSO
+WHERE ID_CURSO = 1;
+
+-- Recien aca podemos borrar el ID_ESTUDIANTE = 1
+DELETE FROM ESTUDIANTE
+WHERE ID_ESTUDIANTE = 1;
+COMMIT;
